541777ea629fcf6ee76db48e98f56e5f4ceb5fbe
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-04-11 12:24:38 +0530

    Create string_casefold.py

---
+++ string_casefold.py
@@ -0,0 +1,9 @@
+#The casefold() method is a method 
+#which c0nverts strings to case folded strings for caseless matching.
+
+string = "PYTHON IS AWESOME"
+
+# print lowercase string
+print("Lowercase string:", string.casefold())
+
+#outputs : python is awesome
