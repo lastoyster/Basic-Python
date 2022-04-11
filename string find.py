064c7c8956966855174ec3547525dccf1bff97fe
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-04-11 12:29:38 +0530

    Create string find.py

---
+++ string find.py
@@ -0,0 +1,9 @@
+#The find() method returns the index of first occurrence of the substring (if found).
+#If not found, it returns -1.
+
+message = 'Python is a fun programming language'
+
+# check the index of 'fun'
+print(message.find('fun'))
+
+# Output: 12
