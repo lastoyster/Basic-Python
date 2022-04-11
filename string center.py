338660312e26ab403d23c66499d55a536634c80f
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-04-11 12:26:04 +0530

    Create string center.py

---
+++ string center.py
@@ -0,0 +1,10 @@
+#The center() method returns a string 
+#which is padded with the specified character.
+
+string = "Python is awesome"
+
+new_string = string.center(24)
+
+print("Centered String: ", new_string)
+
+#outputs: Centered String:     Python is awesome  
